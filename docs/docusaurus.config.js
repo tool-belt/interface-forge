5f44ef689fe36cd3cd295cdd1126e9d3ecc71bf6
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2021-07-24 21:36:01 +0200

    update docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -3,18 +3,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 const title = 'Interface Forge';
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
     title,
     tagline: 'Graceful mock data and fixtures generation using TypeScript.',
     url: 'https://goldziher.github.io',
     baseUrl: '/interfaceForge/',
+    customDocsPath: 'docs',
     projectName: 'interfaceForge',
     organizationName: 'Goldziher',
     trailingSlash: false,
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-    /* favicon: 'img/favicon.ico', */
     themeConfig: {
         navbar: {
             title,
@@ -26,35 +25,10 @@ module.exports = {
                     position: 'left',
                     label: 'Docs',
                 },
-                // { type: 'search', position: 'right' }, // @docusaurus/theme-search-algolia
                 {
                     href: 'https://github.com/Goldziher/interfaceForge',
                     label: 'GitHub',
                     position: 'right',
-                },
-            ],
-        },
-        footer: {
-            style: 'light',
-            /* logo: {
-                alt: 'interfaceForge Open Source Logo',
-                src: 'img/logo.svg',
-                href: 'https://github.com/Goldziher/interfaceForge',
-            }, */
-            copyright: `Interface Forge ${new Date().getFullYear()}. Built with Docusaurus.`,
-            links: [
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'Tweet',
-                            href: 'https://www.twitter.com/share?url=https://github.com/Goldziher/interfaceForge',
-                        },
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/facebook/docusaurus',
-                        },
-                    ],
                 },
             ],
         },
